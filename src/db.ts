f112502a315d0574ac46e049970540ce974c8fd4
ian <ianbalijawa16@gmail.com>
2022-04-06 15:34:11 +0300

    (security:fix) remove hard coded secrets in mongo uri

--- src/db.ts
+++
@@ -1,2 +0,0 @@
-export const MONGO_URI =
-	'mongodb+srv://ianbalijawa:ianbalijawa@cluster0.laxgb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
 import mongoose from 'mongoose';
 import { app } from './app';
-import { MONGO_URI } from './db';
 
 const start = async () => {
 	if (!process.env.JWT_KEY) {
 		throw new Error('JWT_KEY must be defined');
 	}
 
-	// if (!process.env.MONGO_URI) {
-	// 	throw new Error('MONGO_URI must be defined');
-	// }
+	if (!process.env.MONGO_USERNAME) {
+		throw new Error('MONGO_USERNAME must be defined');
+	}
+	if (!process.env.MONGO_PASSWORD) {
+		throw new Error('MONGO_PASSWORD must be defined');
+	}
 
 	try {
-		await mongoose.connect(MONGO_URI);
+		await mongoose.connect(
+			`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.laxgb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+		);
 
 		console.log('Connected to MongoDB');
 	} catch (err) {
