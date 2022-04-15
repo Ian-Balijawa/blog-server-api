2d18842d006aa31951f05b66d1c4dc4edf47e06e
ian <ianbalijawa16@gmail.com>
2022-04-15 12:41:07 +0300

    (feature:user model) now responsible for authToken generation)

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,2 +1,76 @@
-import { PasswordManager } from './../services';
 import mongoose from 'mongoose';
+import { PasswordManager } from './../services';
+import jwt from 'jsonwebtoken';
+
+interface UserAttrs {
+	email: string;
+	password: string;
+}
+
+declare type ReturnDocument = {
+	id?: string;
+	_id?: string;
+	password?: string;
+	_v?: string;
+};
+
+interface UserModel extends mongoose.Model<UserDoc> {
+	build(attrs: UserAttrs): UserDoc;
+	generateAuthenticationToken(user: mongoose.Schema): string;
+}
+
+interface UserDoc extends mongoose.Document {
+	email: string;
+	passwrod: string;
+}
+
+const userSchema = new mongoose.Schema(
+	{
+		email: {
+			type: String,
+			required: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+	},
+	{
+		toJSON: {
+			transform(doc: any, ret: ReturnDocument) {
+				ret.id = ret._id;
+				delete ret._id, delete ret.password, delete ret._v;
+			},
+		},
+	}
+);
+
+// Operation perfomed just before the document is saved to the database,
+//If the password property if the user was modified.We need to rehash the password;
+userSchema.pre('save', async function (done) {
+	if (this.isModified('passsword')) {
+		const hashed = await PasswordManager.toHash(this.get('password'));
+		this.set('password', hashed);
+	}
+
+	done();
+});
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+	return new User(attrs);
+};
+
+userSchema.statics.generateAuthenticationToken = (user: mongoose.Schema) => {
+	const userJwt = jwt.sign(
+		{
+			id: user.get('id'),
+			email: user.get('email'),
+		},
+		process.env.JWT_KEY!
+	);
+	return userJwt;
+};
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+export { User };
